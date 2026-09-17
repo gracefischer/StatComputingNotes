b82bd2a2a5dafd69a9f81c321c12a48abfbcd204
gracefischer <gkfischer@loyola.edu>
2026-09-17 10:14:32 -0400

    Download colorblindr package from github

--- Lecture_Notes/Lecture_5/class_example_ggplot.R
+++ Lecture_Notes/Lecture_5/class_example_ggplot.R
@@ -6,6 +6,9 @@ remotes::install_github("bbc/bbplot")
 ## Install ThemePark from GitHub
 remotes::install_github("MatthewBJane/theme_park")
 
+## Install  visual impairment github package colorblindr
+remotes::install_github("clauswilke/colorblindr")
+
 ## Now that we are familiar with palmerpenguins let’s make a scatter plot with flipper_length_mm on the x-axis, bill_length_mm on the y-axis, colored by species, and a smoother by adding a linear regression.
 
 ## Need to fix, review class lecture
