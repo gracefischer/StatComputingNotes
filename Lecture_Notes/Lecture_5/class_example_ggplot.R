c10c8ec94b303a778127507994de1f0f5eed273f
gracefischer <gkfischer@loyola.edu>
2026-09-17 10:02:05 -0400

    See the class lecture notes about the try yourseld / in class example

---
+++ Lecture_Notes/Lecture_5/class_example_ggplot.R
@@ -0,0 +1,19 @@
+library("tidyverse")
+library("palmerpenguins")
+
+remotes::install_github("bbc/bbplot")
+
+
+## Now that we are familiar with palmerpenguins let’s make a scatter plot with flipper_length_mm on the x-axis, bill_length_mm on the y-axis, colored by species, and a smoother by adding a linear regression.
+
+## Need to fix, review class lecture
+penguins |>
+  ggplot(
+    aes(x = flipper_length_mm, y = bill_length_mm),
+    color = species
+  ) +
+  geom_point() +
+  geom_smooth(method = "lm")
+library(palmerpenguins)
+penguins +
+  theme_dark(base_family = "Times")
